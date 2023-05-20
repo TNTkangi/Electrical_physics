a1b6c6abf28d6149a20e915bf4d475b68183e5bb
TNTkangi <eertyyyggghcv25488@gmail.com>
2023-05-20 12:21:50 +0700

    Type 1.1_s

--- Kiji.cpp
+++ Kiji.cpp
@@ -6,11 +6,23 @@ class Kiji {
     public:
         void control() {
          double E = 1.60e-19;
+         int T;
          double  N[5];
-            cout << "Input electron count: ";
-            cin >> N[0];
-            cout << "\n_______________________________\n";
-            cout << "q: " << N[0] * E << endl;
+                    cout << "Electron count Input 1" << endl;
+                    cout << "\n_______________________________\n";
+                    cout << "Input Function number: " ;
+                    cin >> T;
+                    cout << "\n_______________________________\n";
+            switch (T) {
+                case  1 :
+                    cout << "Input electron count: ";
+                    cin >> N[0];
+                    cout << "\n_______________________________\n";
+                    cout << "q: " << N[0] * E << endl;
+                break;
+                case 2 :
+                    
+            }
 
     }
 };
