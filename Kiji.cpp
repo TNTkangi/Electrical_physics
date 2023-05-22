37f06754a9cbab2318ab72792da925facefb2069
TNTkangi <eertyyyggghcv25488@gmail.com>
2023-05-22 19:29:20 +0700

    Type 1.3

--- Kiji.cpp
+++ Kiji.cpp
@@ -11,6 +11,8 @@ class Kiji {
          double  N[10];
                     cout << "Electron count Input 1" << endl;
                     cout << "\n_______________________________\n";
+                    cout << "Force of electron Input 2" << endl;
+                    cout << "\n_______________________________\n";
                     cout << "Input Function number: " ;
                     cin >> T;
                     cout << "\n_______________________________\n";
@@ -31,18 +33,27 @@ class Kiji {
                 N[4] = pow(N[3],2);
                      cout << "F: " << (K * N[1] * N[2]) / N[4] << "N" << endl;
                 break;
+                case 3 : 
+                   cout << "Foce 1: ";
+                   cin >> N[5];
+                   cout << "Foce 2: ";
+                   cin >> N[6];
+                N[7]=(pow(N[5],2)) + (pow(N[6],2));
+                sqrt (N[7]);
+                cout << N[7] << endl;
                 default : 
                     cout << "Please new input number" << endl;
-                    // sqrt (n[]);
+                    
             }
                     cout << "\n_______________________________\n";
                     cout << "End program" << endl;
                     cout << "_________________________________\n"; 
     }
-};
+}; 
 
 int main () {
     Kiji Ki1;
     Ki1.control();
        return 0;
 }
+/*ถ้ามียกกำลังของ 10 ให้ใส่ตัว e ตามด้วยเลขยกกำลัง เช่น 1e+5 = 1 คูณ ( 10 ยกกำลัง 5 )*/
