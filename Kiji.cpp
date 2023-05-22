db013f45875b138370a444fcf053b69a785213f0
TNTkangi <eertyyyggghcv25488@gmail.com>
2023-05-22 18:31:30 +0700

    Type_1.2

--- Kiji.cpp
+++ Kiji.cpp
@@ -7,7 +7,8 @@ class Kiji {
         void control() {
          double E = 1.60e-19;
          int T;
-         double  N[5];
+         double K = 9e9;
+         double  N[10];
                     cout << "Electron count Input 1" << endl;
                     cout << "\n_______________________________\n";
                     cout << "Input Function number: " ;
@@ -21,9 +22,22 @@ class Kiji {
                     cout << "q: " << N[0] * E << endl;
                 break;
                 case 2 :
-                    
+                     cout << "Input q1: ";
+                     cin >> N[1];
+                     cout << "Input q2: ";
+                     cin >> N[2];
+                     cout << "Input r(m): ";
+                     cin >> N[3];
+                N[4] = pow(N[3],2);
+                     cout << "F: " << (K * N[1] * N[2]) / N[4] << "N" << endl;
+                break;
+                default : 
+                    cout << "Please new input number" << endl;
+                    // sqrt (n[]);
             }
-
+                    cout << "\n_______________________________\n";
+                    cout << "End program" << endl;
+                    cout << "_________________________________\n"; 
     }
 };
 
